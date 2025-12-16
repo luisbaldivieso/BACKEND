c29d02337a1352ac6c497491ad00bfd4ca8ee652
Luis-bal <baldiviesoviadezp@gmail.com>
2025-12-16 16:08:47 -0400

    ajustesmongo

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,20 +1,21 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+
 import { UsuarioController } from './controllers/usuario.controller';
 import { CuentaController } from './controllers/cuenta.controller';
 import { TransaccionController } from './controllers/transaccion.controller';
+
 import { UsuarioService } from './services/usuario.service';
 import { CuentaService } from './services/cuenta.service';
 import { TransaccionService } from './services/transaccion.service';
+
 import { UsuarioSchema } from './schemas/usuario.schema';
 import { CuentaSchema } from './schemas/cuenta.schema';
 import { TransaccionSchema } from './schemas/transaccion.schema';
 
 @Module({
   imports: [
-    MongooseModule.forRoot(process.env.MONGO_URI || 'mongodb://localhost/nest'),
+    MongooseModule.forRoot('mongodb://localhost:27017/app_banco'),
     MongooseModule.forFeature([
       { name: 'Usuario', schema: UsuarioSchema },
       { name: 'Cuenta', schema: CuentaSchema },
@@ -22,11 +23,14 @@ import { TransaccionSchema } from './schemas/transaccion.schema';
     ]),
   ],
   controllers: [
-    AppController,
     UsuarioController,
     CuentaController,
     TransaccionController,
   ],
-  providers: [AppService, UsuarioService, CuentaService, TransaccionService],
+  providers: [
+    UsuarioService,
+    CuentaService,
+    TransaccionService,
+  ],
 })
 export class AppModule {}
